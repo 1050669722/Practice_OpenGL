299d233a1290ee91800cbf9220dd51201a3f0b21
1050669722 <1050669722@qq.com>
2024-05-29 23:48:39 +0800

    Initialize Project;

---
+++ main.cpp
@@ -0,0 +1,36 @@
+﻿//
+// Created by ASUS on 2024/5/13.
+//
+
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello OpenGL!" << std::endl;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // MacOS
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    return 0;
+}
